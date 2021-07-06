af7f666df37ad0e8d2111961f6da619ac89e4356
joaolobao380 <victor_jv380@hotmail.com>
2021-07-05 22:58:48 -0300

    configure eslint and test library

--- __tests__/App.js
+++ __tests__/App.js
@@ -2,9 +2,9 @@ import 'react-native';
 import React from 'react';
 import App from '../App.tsx';
 
-// Note: test renderer must be required after react-native.
+// Note: test renderer must be required aftser react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+    renderer.create(<App />);
 });
---
+++ src/global/styles/theme.ts
@@ -0,0 +1,9 @@
+export default {
+    colors: {
+        background: '#F4F4F4',
+        textPrimary: '#595555',
+        textLight: 'rgba(89, 85, 85, 0.38)',
+        secondary: '#B70DF3',
+        secondaryLight: 'rgba(201, 64, 249, 0.27)',
+    },
+};
